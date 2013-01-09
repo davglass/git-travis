f1f7856e937042282f4f7e20c2564aef63f8d011
Dav Glass <davglass@gmail.com>
2013-01-09 13:39:14 -0600

    License Updates

--- bin/git-travis.js
+++ bin/git-travis.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
 
+/*
+Copyright (c) 2012, Yahoo! Inc. All rights reserved.
+Code licensed under the BSD License:
+http://yuilibrary.com/license/
+*/
+
 var travis = require('../lib/');
 
 travis.info(function(user, repo, branch) {
